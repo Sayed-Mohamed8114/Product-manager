603502ef5e9e43654166a6d192090b5c429d9e89
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-07-30 17:13:43 +0300

    dealing with get product by id and post porduct

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,status
 from models.products_model import Product
 
 app = FastAPI()
@@ -27,3 +27,19 @@ products = [
 @app.get("/get_all_products")
 async def get_all_products():
     return products
+
+@app.get("/Product/{id}")
+async def get_by_id(id:int):
+    for product in products:
+        if product.id == id:
+            return product 
+        else:
+            return "product not found"
+
+@app.post("/product",status_code=status.HTTP_201_CREATED,summary="product creation",tags=["products"])
+async def create_item(product:Product):
+    try:   
+        products.append(product)
+        return "created successfully"
+    except:
+        return "error happened"
