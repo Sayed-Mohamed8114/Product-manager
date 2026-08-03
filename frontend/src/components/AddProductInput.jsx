77a03237402c0b7ab03be2a956aadf452640404a
Sayed-Mohamed8114 <sayed.Mohamed8114@gmail.com>
2026-08-03 18:55:25 +0300

    add new edits in product table for design

---
+++ frontend/src/components/AddProductInput.jsx
@@ -0,0 +1,11 @@
+import {addProduct} from '../services/products'; 
+
+
+export default function AddProductInput() {
+    return (
+        <div>
+            <input type="text"/>
+            <button></button>
+        </div>
+    )
+}
--- frontend/src/components/ProductsTable.jsx
+++ frontend/src/components/ProductsTable.jsx
@@ -15,31 +15,42 @@ export default function ProductsTable() {
   useEffect(() => {
     fetchProdcuts();
   }, []);
-
+  const table_head_style = `text-center text-slate-200 font-bold text-lg w-auto px-2 py-2 cursor-pointer hover:underline hover:text-slate-900 duration-700 transition `;
+  const table_row_style = `text-center text-slate-100 font-extrabold text-sm px-3 py-1`;
   return (
     <table
       border="1"
-      cellPadding="10"
-      cellSpacing="10"
-      className="bg-white/15 backdrop-blur-2xl h-auto w-[50%] text-slate-50 rounded-md"
+      className="bg-white/15 backdrop-blur-2xl h-auto w-[70%] text-slate-50 rounded-lg  shadow-sm shadow-slate-800"
     >
-      <thead className="">
-        <tr className="">
-          <th>ID</th>
-          <th>Name</th>
-          <th>Description</th>
-          <th>Price</th>
-          <th>Quantity</th>
+      <thead>
+        <tr>
+          <th className={table_head_style}>ID</th>
+          <th className={table_head_style}>Name</th>
+          <th className={table_head_style}>Description</th>
+          <th className={table_head_style}>Price</th>
+          <th className={table_head_style}>Quantity</th>
+          <th className={table_head_style}>edit product</th>
+          <th className={table_head_style}>Delete product</th>
         </tr>
       </thead>
       <tbody>
         {products.map((product) => (
-          <tr key={product.id}>
-            <td className="text-center">{product.id}</td>
-            <td className="text-center">{product.name}</td>
-            <td className="text-center">{product.description}</td>
-            <td className="text-center">${product.price}</td>
-            <td className="text-center">{product.quantity}</td>
+          <tr key={product.id} className="m-2">
+            <td className={table_row_style}>{product.id}</td>
+            <td className={table_row_style}>{product.name}</td>
+            <td className={table_row_style}>{product.description}</td>
+            <td className={table_row_style}>${product.price}</td>
+            <td className={table_row_style}>{product.quantity}</td>
+            <td className="text-center px-2 py-1 ">
+              <button className="bg-green-600 rounded-lg p-1 cursor-pointer w-[70%] text-white hover:text-green-100 hover:bg-green-900 transition duration-700">
+                Edit
+              </button>
+            </td>
+            <td className="text-center px-2 py-1 ">
+              <button className="bg-red-700 rounded-lg p-1 cursor-pointer w-[70%] text-white hover:text-green-100 hover:bg-red-900 transition duration-700">
+                Delete
+              </button>
+            </td>
           </tr>
         ))}
       </tbody>
